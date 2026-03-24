56bcb97d30fdad920dc449cb2182142bd30d373a
Shubham Kadam <shubhamkadam22100@gmail.com>
2026-03-24 21:20:10 +0530

    update commit

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+const express = require("express");
+const app = express();
+
+
+// create connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Shubham@2210',
+  database: 'delta_app',
+  password: "Shubham@2210"
+});
 
 let getRandomUser = () => {
-  return {
-    userId: faker.string.uuid(),
-    username: faker.internet.username(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
+  return [
+     faker.string.uuid(),
+     faker.internet.username(),
+     faker.internet.email(),
+     faker.internet.password(),
     
-  };
-}
+  ];
+};
 
 
-console.log(getRandomUser());
+//home route 
+ 
+app.get("/", (req, res) => {
+  let q = "SELECT count(*) AS total FROM user";
+
+  connection.query(q, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.send("Database Error");
+    }
+
+    console.log(result);
+
+    res.send(result); // ✅ better than res.send
+  });
+});
+
+// show user route 
+
+app.get("/user", (req, res) => {
+    let q = "SELECT * FROM users";
+    connection.query(q, (err, users) => {
+    res.render("showusers.ejs", {users});
+    });
+});
+
+app.listen("8080" , () => {
+    console.log("server is on")
+});
+
